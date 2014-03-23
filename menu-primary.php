f757f5f571b474fb34d679807dbcec5d186308af
BenSibley <ben@competethemes.com>
2014-03-23 09:22:21 -0400

    removed if statement stopping unset menu fallback from showing

--- menu-primary.php
+++ menu-primary.php
@@ -1,17 +1,13 @@
-<?php if ( has_nav_menu( 'primary' ) ) : ?>
+<i class="fa fa-bars toggle-button"></i>
 
-	<i class="fa fa-bars toggle-button"></i>
-		
-	<div class='menu-slider'>
-		<div class='site-info'>	
-			<?php echo get_avatar(get_bloginfo('admin_email')); ?>
-			<p><?php echo bloginfo('description'); ?></p>
-			<hr />
-		</div>
-		<div class="menu-container menu-primary">
-			<?php wp_nav_menu( array( 'theme_location' => 'primary', 'container_class' => 'menu', 'menu_class' => 'menu-primary-items', 'menu_id' => 'menu-primary-items', 'fallback_cb' => 'wp_page_menu' ) ); ?>
-		</div><!-- #menu-primary .menu-container -->
-		<?php ct_social_media_icons(); // adds social media icons ?>
-	</div>
-	
-<?php endif; ?>
+<div class='menu-slider'>
+    <div class='site-info'>
+        <?php echo get_avatar(get_bloginfo('admin_email')); ?>
+        <p><?php echo bloginfo('description'); ?></p>
+        <hr />
+    </div>
+    <div class="menu-container menu-primary">
+        <?php wp_nav_menu( array( 'theme_location' => 'primary', 'container_class' => 'menu', 'menu_class' => 'menu-primary-items', 'menu_id' => 'menu-primary-items', 'fallback_cb' => 'wp_page_menu' ) ); ?>
+    </div><!-- #menu-primary .menu-container -->
+    <?php ct_social_media_icons(); // adds social media icons ?>
+</div>
