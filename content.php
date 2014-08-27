2ad7bca3b357da0e1c6e5b44299bc3c0c6d28311
BenSibley <ben@competethemes.com>
2014-08-27 11:35:45 -0400

    internationalized dates

--- content.php
+++ content.php
@@ -5,7 +5,7 @@ if( is_home() ) { ?>
     	<?php ct_drop_featured_image(); ?>
     	<div class='excerpt-meta'>
 			<span class='excerpt-date updated' itemprop="datePublished" content="<?php echo get_the_date('Y-m-d'); ?>">
-				<?php echo get_the_date('d M Y'); ?>
+				<?php echo date_i18n( get_option( 'date_format' ), strtotime( get_the_date() ) ); ?>
 			</span>
 			<span itemprop="author" itemscope itemtype="http://schema.org/Person" class='excerpt-author'>
 				<span><?php _e('Published by:','drop'); ?></span>
@@ -36,7 +36,7 @@ if( is_home() ) { ?>
                 <span class="vcard author" itemprop="author" itemscope itemtype="http://schema.org/Person"><?php _e('Published by ','drop'); ?>
                     <a class="fn" href="<?php esc_url(get_author_posts_url(get_the_author_meta('ID'))) ?>" itemprop="name"><?php the_author_meta( 'display_name' ); ?></a>
                 </span> on
-                <span class="updated" itemprop="datePublished" content="<?php echo get_the_date('Y-m-d'); ?>"><?php the_date('M j, Y'); ?></span>
+                <span class="updated" itemprop="datePublished" content="<?php echo get_the_date('Y-m-d'); ?>"><?php echo date_i18n( get_option( 'date_format' ), strtotime( get_the_date() ) ) ?></span>
             </p>
 		</div>
 		<div class='entry-header'>
