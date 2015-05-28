28d03e67ca398e9fed2268e615a15401516dba79
BenSibley <ben@competethemes.com>
2015-05-28 10:48:59 -0400

    removed theme upgrade and customizer reset sections

--- theme-options.php
+++ theme-options.php
@@ -26,36 +26,12 @@ function ct_drop_options_content(){
                 <a target="_blank" class="button-primary" href="https://www.competethemes.com/documentation/drop-support-center/"><?php _e('Visit Support Center', 'drop'); ?></a>
             </p>
         </div>
-        <div class="content content-premium-upgrade">
-            <h3><?php _e('Upgrade to Drop Pro', 'drop'); ?></h3>
-            <p><?php _e('Drop Pro is the premium upgrade for Drop. It has custom colors, new layouts, background images, and more', 'drop'); ?>...</p>
-            <p>
-                <a target="_blank" class="button-primary" href="https://www.competethemes.com/drop-pro/"><?php _e('See Full Feature List', 'drop'); ?></a>
-            </p>
-        </div>
         <div class="content content-resources">
             <h3><?php _e('WordPress Resources', 'drop'); ?></h3>
             <p><?php _e('Save time and money searching for WordPress products by following our recommendations', 'drop'); ?>.</p>
             <p>
                 <a target="_blank" class="button-primary" href="https://www.competethemes.com/wordpress-resources/"><?php _e('View Resources', 'drop'); ?></a>
             </p>
-        </div>
-        <div class="content content-delete-settings">
-            <h3><?php _e('Reset Customizer Settings', 'drop'); ?></h3>
-            <p>
-                <?php
-                $url = admin_url('customize.php');
-                $text = sprintf( __( '<strong>Warning:</strong> Clicking this button will erase your current settings in the <a href="%s">Customizer</a>', 'drop' ), esc_url( $url ) );
-                echo $text . ".";
-                ?>
-            </p>
-            <form method="post">
-                <input type="hidden" name="drop_reset_customizer" value="drop_reset_customizer_settings" />
-                <p>
-                    <?php wp_nonce_field( 'drop_reset_customizer_nonce', 'drop_reset_customizer_nonce' ); ?>
-                    <?php submit_button( __( 'Reset Customizer Settings', 'drop' ), 'delete', 'delete', false ); ?>
-                </p>
-            </form>
         </div>
         <?php hybrid_do_atomic( 'theme_options_after' ); ?>
     </div>
